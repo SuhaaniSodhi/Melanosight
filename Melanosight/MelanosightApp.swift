b1519cd8a40141d502563b9790a4db878a3ee393
Suhaani Sodhi <suhaani.sodhi@gmail.com>
2024-09-21 09:34:05 -0700

    Initial Commit

---
+++ Melanosight/MelanosightApp.swift
@@ -0,0 +1,17 @@
+//
+//  MelanosightApp.swift
+//  Melanosight
+//
+//  Created by Suhaani Sodhi on 9/21/24.
+//
+
+import SwiftUI
+
+@main
+struct MelanosightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
