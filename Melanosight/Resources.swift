de0a261d287d8ca26c6810704f1d7280b387c589
Suhaani Sodhi <suhaani.sodhi@gmail.com>
2024-09-21 23:00:49 -0700

    classifying page day 1

--- Melanosight/Resources.swift
+++ Melanosight/Resources.swift
@@ -9,9 +9,27 @@ import SwiftUI
 
 struct Resources: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack (){
+            Text("Welcome to our Melanoma Classifier!")
+                .font(.largeTitle)
+                .foregroundColor(Color(red: 0.7, green: 0.4, blue: 0.8))
+                .padding(.bottom)
+            Text("Take or upload a picture of your suspicious lesions or moles to check for melanoma.")
+            Button(action: {
+                // Action for Classify button
+            }) {
+                Text("Classify!")
+                    .font(.title2)
+                    .padding()
+                    .frame(width: 150.0)
+                    .background(Color.purple)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+                    .padding(.horizontal)
+            }
+        }
     }
-}
+    }
 
 #Preview {
     Resources()
