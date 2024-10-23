c75da4d398f8d5d34b7857fdf6b874f7a8fc49c1
Suhaani Sodhi <suhaani.sodhi@gmail.com>
2024-10-22 17:06:40 -0700

    Pixel Buffer

--- Melanosight/Classifier.swift
+++ Melanosight/Classifier.swift
@@ -8,6 +8,31 @@
 import SwiftUI
 import CoreML
 
+extension UIImage {
+    func toCVPixelBuffer() -> CVPixelBuffer? {
+        let attrs = [kCVPixelBufferCGImageCompatibilityKey: kCFBooleanTrue, kCVPixelBufferCGBitmapContextCompatibilityKey: kCFBooleanTrue] as CFDictionary
+        var pixelBuffer : CVPixelBuffer?
+        let status = CVPixelBufferCreate(kCFAllocatorDefault, Int(self.size.width), Int (self.size.height), kCVPixelFormatType_32ARGB, attrs, &pixelBuffer)
+        guard (status == kCVReturnSuccess) else {
+            return nil
+        }
+        CVPixelBufferLockBaseAddress(pixelBuffer!, CVPixelBufferLockFlags(rawValue: 0))
+        let pixelData = CVPixelBufferGetBaseAddress(pixelBuffer!)
+        
+        let rgbColorSpace = CGColorSpaceCreateDeviceRGB()
+        let context = CGContext(data: pixelData, width: Int(self.size.width), height: Int (self.size.height), bitsPerComponent: 8, bytesPerRow: CVPixelBufferGetBytesPerRow(pixelBuffer!), space: rgbColorSpace, bitmapInfo:
+        CGImageAlphaInfo.noneSkipFirst.rawValue)
+        
+        context?.translateBy(x: 0, y: self.size.height)
+        context?.scaleBy(x: 1.0, y: -1.0)
+        
+        UIGraphicsPushContext(context!)
+        self.draw(in: CGRect(x: 0, y: 0, width: self.size.width, height: self.size.height))
+        UIGraphicsPopContext()
+        CVPixelBufferUnlockBaseAddress (pixelBuffer!, CVPixelBufferLockFlags(rawValue: 0))
+    }
+}
+
 struct Classifier: View {
     
     @State private var Answer = ""
@@ -16,7 +41,15 @@ struct Classifier: View {
     @State private var sourceType: UIImagePickerController.SourceType = .photoLibrary
     
     var imageClassifier: MelClass_1?
-        
+  
+    init(){
+        do{
+            imageClassifier = try MelClass_1(configuration: <#T##MLModelConfiguration#>)
+        } catch{
+            print(error)
+        }
+    }
+
     var body: some View {
         NavigationView{
             ZStack{
@@ -38,7 +71,7 @@ struct Classifier: View {
                         .frame(width: 400.0)
                   
                     Image(uiImage: selectedImage ?? UIImage(named: "yourImageNameHere")!)
-                    
+               
                     HStack(){
                         Button(action: {
                             self.sourceType = .photoLibrary
@@ -57,7 +90,17 @@ struct Classifier: View {
                     .frame(width: 350.0)
                   
                     Button(action: {
-                        Answer = "Melanoma"
+                        guard let uiImage = UIImage(named: "yourImageNameHere") else{
+                            return
+                        }
+                        
+                        guard let pixelBuffer = selectedImage?.toCVPixelBuffer() else { return }
+                        do {
+                            let result = try imageClassifier!.prediction(image: pixelBuffer)
+                            Answer = result?.classLabel??
+                        } catch{
+                            print(error)
+                        }
                     }) {
                         Text("Classify!")
                             .font(.title2)
